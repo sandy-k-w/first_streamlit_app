919506cc9cf7b770dec95cdaa6c279b02877504a
sandy-k-w <121742961+sandy-k-w@users.noreply.github.com>
2023-01-03 21:51:22 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,11 +33,11 @@ def get_fruityvice_data(this_fruit_choice):
     
  #New section to display fruitvice results-
 streamlit.header("Fruityvice Fruit Advice!")
-  try:
-    fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-    if not fruit_choice:
-      streamlit.error("Please select a fruit to get information")
-  else:
+      try:
+            fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+            if not fruit_choice:
+                  streamlit.error("Please select a fruit to get information")
+      else:
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(fruityvice_normalized)
 
